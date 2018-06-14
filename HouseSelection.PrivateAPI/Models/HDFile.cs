bb88366df0577266776bb453237b538efaab675c
JiangTianChun <RockJiang@cuscapi.com>
2018-06-14 23:44:21 +0800

    上传图片和声音接口

---
+++ HouseSelection.PrivateAPI/Models/HDFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HouseSelection.Model;
+
+namespace HouseSelection.PrivateAPI.Models
+{
+    public class HDFile : BaseResultEntity
+    {
+        public HDFile(string name, string url, string size)
+        {
+            //Name = name;
+            //Url = url;
+            //Size = size;
+            Code = 0;
+            ErrMsg = "";
+        }
+
+        //public string Name { get; set; }
+
+        //public string Url { get; set; }
+
+        //public string Size { get; set; }
+    }
+}
---
+++ HouseSelection.PrivateAPI/Models/WithExtensionMultipartFormDataStreamProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using HouseSelection.LoggerHelper;
+
+namespace HouseSelection.PrivateAPI.Models
+{
+    public class WithExtensionMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public WithExtensionMultipartFormDataStreamProvider(string rootPath,int bufferSize)
+            : base(rootPath, bufferSize)
+        {
+        }
+
+        public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+        {
+            Logger.LogInfo("Upload File:" + Path.GetFileName(GetValidFileName(headers.ContentDisposition.FileName)).ToString(), "", "");
+            //string extension = !string.IsNullOrWhiteSpace(headers.ContentDisposition.FileName) ? Path.GetExtension(GetValidFileName(headers.ContentDisposition.FileName)) : "";
+            //return Guid.NewGuid().ToString() + extension;
+            return Path.GetFileName(GetValidFileName(headers.ContentDisposition.FileName));
+        }
+
+        private string GetValidFileName(string filePath)
+        {
+            char[] invalids = System.IO.Path.GetInvalidFileNameChars();
+            return String.Join("_", filePath.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+        }
+    }
+}
