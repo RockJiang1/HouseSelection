4288e9a15b7f44660c9c17843b84e78a0b3d046d
mmmmeng <ymbmwork@foxmail.com>
2018-06-16 09:04:38 +0800

    提交 电话 - 获取分组电话拨打情况列表 并更新文档

---
+++ HouseSelection.FrontEndAPI/Models/PhoneCallRequest/GetSubscriberDialingStatusRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseSelection.FrontEndAPI.Models.PhoneCallRequest
+{
+    public class GetSubscriberDialingStatusRequestModel
+    {
+        public int ProjectId { get; set; }
+
+        public int GroupId { get; set; }
+
+        public int BeginNumber { get; set; }
+
+        public int EndNumber { get; set; }
+    }
+}
---
+++ HouseSelection.FrontEndAPI/Models/PhoneCallResult/GetSubscriberDialingStatusResultEntity.cs
@@ -0,0 +1,44 @@
+﻿using HouseSelection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HouseSelection.FrontEndAPI.Models.PhoneCallResult
+{
+    public class GetSubscriberDialingStatusResultEntity : BaseResultEntity
+    {
+        /// <summary>
+        /// 未拨打列表
+        /// </summary>
+        public List<UndialedInfo> Undialed { get; set; }
+
+        /// <summary>
+        /// 未接通列表
+        /// </summary>
+        public List<NotconnectedInfo> Notconnected { get; set; }
+    }
+
+    public class UndialedInfo
+    {
+        public int Sequence { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+    }
+
+    public class NotconnectedInfo
+    {
+        public int Sequence { get; set; }
+
+        public string Name { get; set; }
+
+        public int CallTimes { get; set; }
+
+        public DateTime LastCallTime { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
