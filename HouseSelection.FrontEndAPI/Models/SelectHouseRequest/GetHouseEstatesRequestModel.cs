d3ffe86331f14b8c51e0068c3e24d914596bacf5
JiangTianChun <RockJiang@cuscapi.com>
2018-06-18 10:10:37 +0800

    现场获取楼盘和登陆接口

---
+++ HouseSelection.FrontEndAPI/Models/SelectHouseRequest/GetHouseEstatesRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseSelection.FrontEndAPI.Models.SelectHouseRequest
+{
+    public class GetHouseEstatesRequestModel
+    {
+        public int ShakingNumberResultID { get; set; }
+    }
+}
