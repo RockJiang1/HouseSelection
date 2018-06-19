9fe69123623278fe73fae389bd0f000744dc90e7
mmmmeng <ymbmwork@foxmail.com>
2018-06-19 22:38:17 +0800

    temp

---
+++ HouseSelection.FrontEndAPI/Controllers/PhoneCall/MarkPhoneCallStatusController.cs
@@ -0,0 +1,21 @@
+﻿using HouseSelection.BLL;
+using HouseSelection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HouseSelection.FrontEndAPI.Controllers.PhoneCall
+{
+    public class MarkPhoneCallStatusController:ApiController
+    {
+        ShakingNumberResultBLL _shakingNumberResultBLL = new ShakingNumberResultBLL();
+
+        public BaseResultEntity Post()
+        {
+            var ret = new BaseResultEntity();
+            return ret;
+        }
+    }
+}
