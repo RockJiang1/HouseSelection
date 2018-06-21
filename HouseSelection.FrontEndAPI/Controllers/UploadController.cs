73c524840b386747199a3be5b82cd2c040f51104
mmmmeng <ymbmwork@foxmail.com>
2018-06-22 00:13:51 +0800

    提交上传文件接口，暂无保存数据逻辑。

---
+++ HouseSelection.FrontEndAPI/Controllers/UploadController.cs
@@ -0,0 +1,57 @@
+﻿using HouseSelection.BLL;
+using HouseSelection.FrontEndAPI.Models;
+using HouseSelection.LoggerHelper;
+using HouseSelection.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace HouseSelection.FrontEndAPI.Controllers
+{
+    public class UploadController:ApiController
+    {
+        ShakingNumberResultBLL _shakingNumberResultBLL = new ShakingNumberResultBLL();
+
+
+        public BaseResultEntity Post(int ShakingResultId)
+        {
+            var ret = new BaseResultEntity()
+            {
+                Code = 0,
+                ErrMsg = ""
+            };
+            try
+            {
+                var shakingInfo = _shakingNumberResultBLL.GetModels(i => i.ID == ShakingResultId).FirstOrDefault();
+                
+                var projectId = shakingInfo.ProjectGroup.ProjectID;
+
+                var file = HttpContext.Current.Request.Files["file"];
+
+                var savePath = ConfigurationManager.AppSettings["UploadPath"].ToString() + projectId.ToString()+"\\";
+                
+                string fileName = file.FileName;
+                if (!Directory.Exists(savePath))
+                {
+                    Directory.CreateDirectory(savePath);
+                }
+
+                file.SaveAs(savePath + file.FileName);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException("上传文件时发生异常！", "UploadController", "Post", ex);
+                ret.Code = 999;
+                ret.ErrMsg = ex.Message;
+            }
+            
+
+            return ret;
+        }
+    }
+}
---
+++ HouseSelection.FrontEndAPI/Models/UploadFileRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseSelection.FrontEndAPI.Models
+{
+    public class UploadFileRequestModel
+    {
+        /// <summary>
+        /// 摇号结果id
+        /// </summary>
+        public int ShakingResultId { get; set; }
+    }
+}
