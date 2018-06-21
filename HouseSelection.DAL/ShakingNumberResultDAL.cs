e3db730616abb2be410a4ae66dd5cb9ffffc77f3
JiangTianChun <466671628@qq.com>
2018-06-21 17:27:36 +0800

    格式化时间

--- HouseSelection.DAL/ShakingNumberResultDAL.cs
+++ HouseSelection.DAL/ShakingNumberResultDAL.cs
@@ -14,7 +14,7 @@ namespace HouseSelection.DAL
             using(HouseSelectionDBEntities DB = new HouseSelectionDBEntities())
             {
                 DateTime? dt = null;
-                return (from SN in DB.ShakingNumberResult
+                return (from Temp in ((from SN in DB.ShakingNumberResult
                            join M in DB.SubscriberProjectMapping on SN.SubscriberProjectMappingID equals M.ID
                            join S in DB.Subscriber on M.SubscriberID equals S.ID
                            join Htmp in DB.HouseSelectPeriod on SN.ID equals Htmp.ShakingNumberResultID into temp
@@ -22,7 +22,7 @@ namespace HouseSelection.DAL
                            where SN.ProjectGroupID == GroupID
                            && SN.SelectHouseSequance >= StartSeq
                            && SN.SelectHouseSequance <= EndSeq
-                           select new DialedInfo
+                           select new
                            {
                                ShakingNumberResultId = SN.ID,
                                ShakingNumberSequance = SN.ShakingNumberSequance,
@@ -41,6 +41,23 @@ namespace HouseSelection.DAL
                                //LastCallTime = (SN.LastUpdate ?? SN.CreateTime).ToString("yyyy-MM-dd HH:mm:ss"),
                                LastCallTime = SN.NoticeTime == 0 ? null : SN.LastUpdate,
                                Phone = S.Telephone
+                           }).ToList())
+                           select new DialedInfo
+                           {
+                               ShakingNumberResultId = Temp.ShakingNumberResultId,
+                               ShakingNumberSequance = Temp.ShakingNumberSequance,
+                               SelectHouseSequence = Temp.SelectHouseSequence,
+                               Name = Temp.Name,
+                               IdentityID = Temp.IdentityID,
+                               BeginTime = Temp.BeginTime == null ? null : ((DateTime)Temp.BeginTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                               EndTime = Temp.EndTime == null ? null : ((DateTime)Temp.EndTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                               CallTimes = Temp.CallTimes,
+                               IsContacted = Temp.IsContacted,
+                               IsCallBack = Temp.IsCallBack,
+                               IsMessageSend = Temp.IsMessageSend,
+                               ResultType = Temp.ResultType,
+                               LastCallTime = Temp.LastCallTime == null ? null : ((DateTime)Temp.LastCallTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                               Phone = Temp.Phone
                            }).ToList();
 
             }
